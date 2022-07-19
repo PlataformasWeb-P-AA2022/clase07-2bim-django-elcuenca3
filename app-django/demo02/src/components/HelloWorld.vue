ee1e67e8da2ce829bc4019569d579a8bf11744b4
elcuenca3 <elcuenca3@utpl.edu.ec>
2022-07-18 19:28:34 -0500

    vue telefonos

---
+++ app-django/demo02/src/components/HelloWorld.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="tasks_container">
+        <div class="tasks_content">
+            <h1>Listado de Estudiantes</h1>
+            <!--ul class="tasks_list"-->
+                <span v-for="telefono in tasks" :key="telefono.id">
+                    <p><b>Telefono:</b> {{ telefono.telefono }}</p>
+                    <p><b>Tipo:</b>{{ telefono.tipo }}</p>
+                    <hr>
+                </span>
+            <!--/ul-->
+        </div>
+    </div>
+  <hr>
+   
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                // tasks
+                tasks: [''],
+                telefono: '',
+                tipo: '',
+            }
+        },
+        methods: {
+            async getData() {
+                try {
+                    // fetch tasks
+                    const response = await this.$http.get('http://localhost:8000/api/numerost/');
+                    // set the data returned as tasks
+                    this.tasks = response.data;
+                } catch (error) {
+                    // log the error
+                    console.log(error);
+                }
+            }
+        },
+        created() {
+            // Fetch tasks on page load
+            this.getData();
+        }
+    }
+</script>
